0e5aaf91a30cfdd11e15b649291573685eaba1a6
JoaquinHoward <falarjoaquinhoward@gmail.com>
2026-09-10 16:42:51 +0800

    Fixed typo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite(['resources/css/app.css', 'resources/app/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 </head> 
 <body class="bg-black h-screen flex justify-center items-center text-white">
